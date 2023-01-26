368dbd7638a22ad1c47fc06a7a9e83778c991fc1
jihyePH <ppjihye0405@gmail.com>
2023-01-26 15:02:28 +0900

    jihye

--- src/main/java/com/mulcam/SpringProject/controller/BasicController.java
+++ src/main/java/com/mulcam/SpringProject/controller/BasicController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/test")
 public class BasicController {
+	
 	
 	@GetMapping("/list")
 	public String list() {
