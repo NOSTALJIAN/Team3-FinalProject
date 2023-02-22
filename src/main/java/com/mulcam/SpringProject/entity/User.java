3fb76888beb03ea58733be6313b999c087c75269
rnaqpddl123@gmail.com <rnaqpddl123@gmail.com>
2023-02-22 15:18:12 +0900

    닉네임 추가

--- src/main/java/com/mulcam/SpringProject/entity/User.java
+++ src/main/java/com/mulcam/SpringProject/entity/User.java
@@ -6,47 +6,50 @@ public class User {
 	private String uid;
 	private String pwd;
 	private String uname;
+	private String nickname;
 	private String uImage;
 	private String phoneNum;
 	private String email;
 	private int emailCheck;
 	private int role;
-	private int isDeleted;
+	private int uIsDeleted;
 	private LocalDate uRegDate;
 	
 	public User() {}
 	/** 회원정보 수정용*/
-	public User(String uid, String phoneNum, String email, int emailCheck) {
+	public User(String uid, String nickname, String phoneNum, String email, int emailCheck) {
 		super();
 		this.uid = uid;
+		this.nickname = nickname;
 		this.phoneNum = phoneNum;
 		this.email = email;
 		this.emailCheck = emailCheck;
 	}
-	public User(String uid, String pwd, String uname, String phoneNum, String email, int emailCheck) {
+	public User(String uid, String pwd, String uname, String nickname, String phoneNum, String email, int emailCheck) {
 		super();
 		this.uid = uid;
 		this.pwd = pwd;
 		this.uname = uname;
+		this.nickname = nickname;
 		this.phoneNum = phoneNum;
 		this.email = email;
 		this.emailCheck = emailCheck;
 	}
-	public User(String uid, String pwd, String uname, String uImage, String phoneNum, String email, int emailCheck,
-			int role, int isDeleted, LocalDate uRegDate) {
+	public User(String uid, String pwd, String uname, String nickname, String uImage, String phoneNum, String email,
+			int emailCheck, int role, int uIsDeleted, LocalDate uRegDate) {
 		super();
 		this.uid = uid;
 		this.pwd = pwd;
 		this.uname = uname;
+		this.nickname = nickname;
 		this.uImage = uImage;
 		this.phoneNum = phoneNum;
 		this.email = email;
 		this.emailCheck = emailCheck;
 		this.role = role;
-		this.isDeleted = isDeleted;
+		this.uIsDeleted = uIsDeleted;
 		this.uRegDate = uRegDate;
 	}
-
 	public String getUid() {
 		return uid;
 	}
@@ -64,6 +67,12 @@ public class User {
 	}
 	public void setUname(String uname) {
 		this.uname = uname;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
 	public String getuImage() {
 		return uImage;
@@ -95,11 +104,11 @@ public class User {
 	public void setRole(int role) {
 		this.role = role;
 	}
-	public int getIsDeleted() {
-		return isDeleted;
+	public int getuIsDeleted() {
+		return uIsDeleted;
 	}
-	public void setIsDeleted(int isDeleted) {
-		this.isDeleted = isDeleted;
+	public void setuIsDeleted(int uIsDeleted) {
+		this.uIsDeleted = uIsDeleted;
 	}
 	public LocalDate getuRegDate() {
 		return uRegDate;
@@ -107,12 +116,13 @@ public class User {
 	public void setuRegDate(LocalDate uRegDate) {
 		this.uRegDate = uRegDate;
 	}
-
 	@Override
 	public String toString() {
-		return "User [uid=" + uid + ", pwd=" + pwd + ", uname=" + uname + ", uImage=" + uImage + ", phoneNum="
-				+ phoneNum + ", email=" + email + ", emailCheck=" + emailCheck + ", role=" + role + ", isDeleted="
-				+ isDeleted + ", uRegDate=" + uRegDate + "]";
+		return "User [uid=" + uid + ", pwd=" + pwd + ", uname=" + uname + ", nickname=" + nickname + ", uImage="
+				+ uImage + ", phoneNum=" + phoneNum + ", email=" + email + ", emailCheck=" + emailCheck + ", role="
+				+ role + ", uIsDeleted=" + uIsDeleted + ", uRegDate=" + uRegDate + "]";
 	}
+
+	
 	
 }
--- src/main/webapp/WEB-INF/view/user/admin.jsp
+++ src/main/webapp/WEB-INF/view/user/admin.jsp
@@ -3,10 +3,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+	<%@ include file="../common/heading.jsp" %>
+	<meta charset="UTF-8">
+	<title>관리자 페이지</title>
 </head>
 <body>
-
+	<%@ include file="../common/top.jsp" %>
+	<button onclick="all()">전체회원</button>
+	<button onclick="normal()">정상회원</button>
+	<button onclick="delete()">탈퇴예정회원</button>
+	<button onclick="exile()">추방회원</button>
+	<table border="1" style="margin: 50px; width: 80%">
+		<tr>
+			<th>UID</th><th>이름</th>
+			<th>email</th><th>전화번호</th>
+			<th>등록일</th><th>탈퇴여부</th>
+		</tr>
+		<c:forEach var="user" items="${userList}">
+		<tr>
+			<td>${user.uid}</td>
+			<td>${user.uname}</td>
+			<td>${user.email}</td>
+			<td>${user.phoneNum}</td>
+			<td>${user.uRegDate}</td>
+			<c:if test="${user.uIsDeleted eq 0}">
+			<td>정상회원</td>	
+			<td><button onclick="">추방하기</button></td>
+			</c:if>
+			<c:if test="${user.uIsDeleted eq 1}">
+			<td>탈퇴예정</td>	
+			<td><button onclick="">탈퇴취소</button></td>
+			</c:if>
+			<c:if test="${user.uIsDeleted eq 2}">
+			<td>추방예정</td>	
+			<td><button onclick="">추방취소</button></td>
+			</c:if>
+		</tr>
+		</c:forEach>
+	</table>
+	<script>
+	
+	</script>
 </body>
 </html>
